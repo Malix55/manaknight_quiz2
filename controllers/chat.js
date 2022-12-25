78c3ae5cb1603ee68ba6ade5477c244c7a3c8032
Malix_55 <abdullah.abid@vultara.com>
2022-12-25 17:09:39 +0500

    90% of the work done

---
+++ controllers/chat.js
@@ -0,0 +1,11 @@
+const redis = require("redis");
+const client = redis.createClient();
+
+const chat = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = chat;
